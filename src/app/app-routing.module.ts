86b8afe52256383dbe7bd0d2c8a02036e7ce7ac7
AndreMyszko <andre.myszko@gmail.com>
2022-02-12 20:44:54 -0300

    add. favicon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,16 @@ import { HomePageComponent } from './page/home-page/home-page.component';
 import { PokemonPageComponent } from './page/pokemon-page/pokemon-page.component';
 
 const routes: Routes = [
-  {path:'', component: HomePageComponent},
-  {path:'pokemon-page', component: PokemonPageComponent}
+  {
+    path:'',
+    component: HomePageComponent,
+    data: {title: 'Pokemon | Home Page'}
+  },
+  {
+    path:'pokemon-page',
+    component: PokemonPageComponent,
+    data: {title: 'Pokemon | Pokemon Page'}
+  }
 ];
 
 @NgModule({
