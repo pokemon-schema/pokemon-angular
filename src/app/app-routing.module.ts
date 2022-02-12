66a5d879e3ff9e1b6c1c46771067aba850ae0801
AndreMyszko <andre.myszko@gmail.com>
2022-02-12 01:57:49 -0300

    add. page diretory final pages migration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './page/home-page/home-page.component';
-import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
+import { PokemonPageComponent } from './page/pokemon-page/pokemon-page.component';
 
 const routes: Routes = [
   {path:'', component: HomePageComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 //Modules Created
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { ItemComponent } from './item/item.component';
-import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
+import { PokemonPageComponent } from './page/pokemon-page/pokemon-page.component';
 import { AbilityComponent } from './ability/ability.component';
 import { TypeComponent } from './type/type.component';
 import { HomePageComponent } from './page/home-page/home-page.component';
